187c85d0bb1774caf99165d888b5490534fcce32
Leul Ayalew <leulayalew1888@gmail.com>
2022-02-05 09:56:46 +0300

    forward and backward controler added

--- src/Controls.tsx
+++ src/Controls.tsx
@@ -10,24 +10,63 @@ type ControlsProps = Pick<
   "isLoading" | "mainColor" | "playerState" | "onReplay"
 > & {
   onPause: () => void;
+  onForward?: () => void;
+  onBackward?: () => void;
 };
 
 const Controls = (props: ControlsProps) => {
-  const { isLoading, mainColor, playerState, onReplay, onPause } = props;
+  const {
+    isLoading,
+    mainColor,
+    playerState,
+    onReplay,
+    onPause,
+    onForward,
+    onBackward,
+  } = props;
   const icon = getPlayerStateIcon(playerState);
+  const goBack = require("./assets/back.png");
+  const goForward = require("./assets/forward.png");
   const pressAction = playerState === PLAYER_STATES.ENDED ? onReplay : onPause;
 
-  const content = isLoading ? (
+  const content = !isLoading ? (
     <ActivityIndicator size="large" color="#FFF" />
   ) : (
-    <TouchableOpacity
-      style={[styles.playButton, { backgroundColor: mainColor }]}
-      onPress={pressAction}
-      accessibilityLabel={PLAYER_STATES.PAUSED ? "Tap to Play" : "Tap to Pause"}
-      accessibilityHint={"Plays and Pauses the Video"}
-    >
-      <Image source={icon} style={styles.playIcon} />
-    </TouchableOpacity>
+    <View style={{ flexDirection: "row" }}>
+      <TouchableOpacity
+        style={[styles.playButton, { backgroundColor: mainColor }]}
+        onPress={onForward}
+        accessibilityLabel={
+          PLAYER_STATES.PAUSED ? "Tap to Play" : "Tap to Pause"
+        }
+        accessibilityHint={"Plays and Pauses the Video"}
+      >
+        <Image source={goBack} style={styles.playIcon} />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[
+          styles.playButton,
+          { backgroundColor: mainColor, marginHorizontal: 8 },
+        ]}
+        onPress={pressAction}
+        accessibilityLabel={
+          PLAYER_STATES.PAUSED ? "Tap to Play" : "Tap to Pause"
+        }
+        accessibilityHint={"Plays and Pauses the Video"}
+      >
+        <Image source={icon} style={styles.playIcon} />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.playButton, { backgroundColor: mainColor }]}
+        onPress={onBackward}
+        accessibilityLabel={
+          PLAYER_STATES.PAUSED ? "Tap to Play" : "Tap to Pause"
+        }
+        accessibilityHint={"Plays and Pauses the Video"}
+      >
+        <Image source={goForward} style={styles.playIcon} />
+      </TouchableOpacity>
+    </View>
   );
 
   return <View style={[styles.controlsRow]}>{content}</View>;
